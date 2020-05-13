1fe685face7dc92edc0a95f5e08d6c68901f0cf8
jankelaa <48515964+jankelaa@users.noreply.github.com>
2020-05-13 21:55:33 +0200

    Create header_gost.php

---
+++ faza6 implementacija/nostradamus/app/Views/sablon/header_gost.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Nostradamus</title>
+    </head>
+    <link rel="stylesheet" href="<?php echo base_url();?>/css.css">
+    <body>
+        <div class="header">
+        <form name="loginform" action="<?php site_url("Gost/loginSubmit") ?>" method="post">
+        <table style="width:100%;" border="0px">
+            <tr>
+                <td rowspan="3" width="60%">
+                    <a href="<?php echo base_url(); ?>"><?php echo img('crystal_ball.png'); ?>
+                    </a></td>
+                <td width="10%">Korisnicko ime</td>
+                <td width="10%">Lozinka</td>
+                <td width="10%"></td>
+            </tr>
+            <tr>
+                <td width="10%"><input type="text" placeholder="Username" name="user"></td>
+                <td width="10%"><input type="password" placeholder="********" name="pass"></td>
+                <td width="10%"><input type="submit" class="button1" value="Uloguj se"></td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div style="float:right">                        
+            <?= anchor("Gost/registracija", "Nemate nalog? Registrujte se!") ?>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;                   
+                </td>
+            </tr>    
+        </table> 
+        </form>
+    </div>
